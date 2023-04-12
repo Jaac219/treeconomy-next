ebba91efb55903ae1eae7e2365dc15864bc6082e
dev16 <dev16@codecraftdev.com>
2023-04-12 17:56:11 -0500

    Danos de kevin

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import BHeader from '../components/blocks/b-header'
+// import 'normalize.css'
+// import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <BHeader />
+      <Component {...pageProps} />
+    </>
+  )
 }
